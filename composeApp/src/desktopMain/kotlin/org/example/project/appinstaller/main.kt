1a835528a2ad5bf7a9e7284c04b39fb04422fb25
Jerónimo Muñoz <jero3000@gmail.com>
2024-11-06 19:04:47 +0100

    Adding an application menu bar

--- composeApp/src/desktopMain/kotlin/org/example/project/appinstaller/main.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/appinstaller/main.kt
@@ -1,19 +1,44 @@
 package org.example.project.appinstaller
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.input.key.KeyShortcut
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.input.key.Key
 import org.example.project.appinstaller.di.appModule
 import org.koin.core.context.startKoin
 
 fun main() = application {
-    startKoin {
-        modules(appModule)
-    }
 
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "AndroidAppInstaller",
-    ) {
-        App()
+    var isOpen by remember { mutableStateOf(true) }
+    if (isOpen) {
+        startKoin {
+            modules(appModule)
+        }
+
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Android application installer",
+        ) {
+            MenuBar {
+                Menu("File", mnemonic = 'F') {
+                    Item("Load configuration...", onClick = { })
+                    Separator()
+                    Item("Settings", onClick = { })
+                    Separator()
+                    Item(
+                        "Exit",
+                        onClick = { isOpen = false },
+                        shortcut = KeyShortcut(Key.Escape),
+                        mnemonic = 'E'
+                    )
+                }
+            }
+            App()
+        }
     }
 }
