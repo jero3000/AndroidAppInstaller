9090a7d289cce4326a426b4ccd1e6add4f19e451
Jerónimo Muñoz <jero3000@gmail.com>
2024-11-11 19:22:51 +0100

    Fixing error writing text to a file. Data must be flushed and the sink closed.

--- composeApp/src/commonMain/kotlin/org/example/project/appinstaller/platform/filesystem/FileSystemImpl.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/appinstaller/platform/filesystem/FileSystemImpl.kt
@@ -5,26 +5,21 @@ import kotlinx.io.IOException
 import kotlinx.io.readString
 import kotlinx.io.writeString
 
-class FileSystemImpl(private val fileUtils: FileUtils) : FileSystem {
+class FileSystemImpl : FileSystem {
 
     override fun readTextFile(file: IPlatformFile): Result<String> {
         return kotlin.runCatching {
-            file.openInputStream()?.readString()
+            file.openInputStream()?.use { it.readString() }
                 ?: throw IOException("File ${file.getPath()} cannot be opened!")
         }
     }
 
     override fun writeTextFile(data: String, file: IPlatformFile): Result<Unit> {
         return kotlin.runCatching {
-            file.openOutputStream(false)?.writeString(data)
-                ?: throw IOException("File ${file.getPath()} cannot be opened!")
-        }
-    }
-
-    private fun readText(path: String): Result<String> {
-        return kotlin.runCatching {
-            val file = fileUtils.getFileFromPath(path, false) ?: throw IOException("File $path not found!")
-            file.openInputStream()?.readString() ?: throw IOException("File $path cannot be opened!")
+            file.openOutputStream(false)?.use {
+                it.writeString(data)
+                it.flush()
+            } ?: throw IOException("File ${file.getPath()} cannot be opened!")
         }
     }
 }
